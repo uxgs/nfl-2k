6cb7a985ecf7317cf3cba1cfb4bdc5b79454e92a
uxgs <gsteratore@gmail.com>
2026-03-14 12:34:50 -0500

    Delete texture-lookup directory

--- texture-lookup/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import AppShell from "./AppShell";
-
-export default function Home() {
-  return <AppShell />;
-}
-
--- texture-lookup/src/lib/data.ts
+++
@@ -1,8 +0,0 @@
-import type { TextureData } from "./types";
-
-import textures from "@/data/textures.json";
-
-export function getTextureData(): TextureData {
-  return textures as TextureData;
-}
-
